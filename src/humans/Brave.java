0fd1078fa0aba731e7e63bf3e10eec3dbec20b2c
AeKanemoto <178535954+kanemotoae@users.noreply.github.com>
2024-09-12 11:34:37 +0900

    課題再提出

--- src/humans/Brave.java
+++ src/humans/Brave.java
@@ -1,7 +1,6 @@
 package humans;
 
 import bases.Human;
-import bases.Living;
 import utils.Dice;
 
 // BraveクラスはHumanクラスを継承する
@@ -12,17 +11,5 @@ public class Brave extends Human {
 		super(name, weapon);
 		this.hp = Dice.get(170, 230); // ヒットポイントを乱数で設定
 		this.offensive = Dice.get(7, 13); // 攻撃力を乱数で設定
-	}
-
-	// attackメソッドのオーバーライド
-	@Override
-	public void attack(Living target) {
-		int damage = Dice.get(1, 10) * this.offensive;
-		target.setHp(target.getHp() - damage);
-		this.offensive--;
-
-		System.out.println("「" + this.getName() + "」" + "が" + "「" + this.getWeapon() + "」" + "で攻撃！" + "「"
-				+ target.getName() + "」" + "に" + damage + "のダメージを与えた。");
-		System.out.println("しかし自分の攻撃力も1減少した。");
 	}
 }
--- src/humans/Wizard.java
+++ src/humans/Wizard.java
@@ -1,7 +1,6 @@
 package humans;
 
 import bases.Human;
-import bases.Living;
 import utils.Dice;
 
 public class Wizard extends Human {
@@ -11,17 +10,5 @@ public class Wizard extends Human {
 		super(name, weapon);
 		this.hp = Dice.get(120, 180); // ヒットポイントを乱数で設定
 		this.offensive = Dice.get(12, 18); // 攻撃力を乱数で設定
-	}
-
-	// attackメソッドのオーバーライド
-	@Override
-	public void attack(Living target) {
-		int damage = Dice.get(1, 10) * this.offensive;
-		target.setHp(target.getHp() - damage);
-		this.offensive--;
-
-		System.out.println("「" + this.getName() + "」" + "が" + "「" + this.getWeapon() + "」" + "で攻撃！" + "「"
-				+ target.getName() + "」" + "に" + damage + "のダメージを与えた。");
-		System.out.println("しかし自分の攻撃力も1減少した。");
 	}
 }
